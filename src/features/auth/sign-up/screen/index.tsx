1b84c4c655bdf3794f51bec5668441801e53fce3
Jan Szolno <jjszolno@gmail.com>
2024-06-04 18:10:17 -0300

    Adds UI components and styling

--- src/features/auth/sign-up/screen/index.tsx
+++ src/features/auth/sign-up/screen/index.tsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, ImageBackground, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import styles from './styles';
 import { SignUpNavigationProps } from './types';
 
-const SignUpScreen: React.FunctionComponent<SignUpNavigationProps> = () => (
-  <View style={styles.container}>
-    <Text>Sign Up</Text>
-  </View>
-);
+const SignUpScreen: React.FunctionComponent<SignUpNavigationProps> = () => {
+  const [email, setEmail] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const onSignUpPress = () => {
+    console.log('Sign Up Pressed');
+  };
+  const onLoginPress = () => {
+    setError('');
+  };
+
+  return (
+    <ImageBackground source={require('assets/auth/background.jpeg')} style={styles.background}>
+      <View style={styles.container}>
+        <Image source={require('assets/auth/logo.png')} style={styles.logo} />
+        <Text style={styles.text}>Email</Text>
+        <TextInput
+          autoComplete="email"
+          autoCapitalize="none"
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Text style={styles.text}>Full Name</Text>
+        <TextInput style={styles.input} value={fullName} onChangeText={setFullName} />
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Text style={styles.text}>Confirm Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+        <TouchableOpacity style={styles.button} onPress={onSignUpPress}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+        <Text style={styles.info}>
+          By signing up, you accept the <Text style={styles.link}>Data Policy.</Text>
+        </Text>
+        {error ? <Text style={styles.error}>{error}</Text> : null}
+        <Text style={styles.info}>
+          Already have an account?{' '}
+          <Text style={styles.link} onPress={onLoginPress}>
+            Log in
+          </Text>
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+};
 
 export default SignUpScreen;
--- src/features/auth/sign-up/screen/styles.ts
+++ src/features/auth/sign-up/screen/styles.ts
@@ -2,9 +2,68 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    padding: 20,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    margin: 30,
+  },
+
+  background: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  text: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  logo: {
+    marginVertical: 20,
+    alignSelf: 'center',
+  },
+
+  input: {
+    marginBottom: 10,
+    height: 40,
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  },
+
+  button: {
+    marginTop: 10,
+    /* grey color when disabled and black when enabled */
+    backgroundColor: '#000000',
+    color: '#000000',
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  buttonText: {
+    color: '#ffffff',
+    textAlign: 'center',
+
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  error: {
+    color: 'red',
+  },
+
+  info: {
+    marginTop: 20,
+    fontSize: 14,
+    textAlign: 'center',
+  },
+
+  link: {
+    color: '#000000',
+    textDecorationLine: 'underline',
   },
 });
 

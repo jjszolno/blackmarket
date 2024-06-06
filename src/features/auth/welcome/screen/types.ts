bff42a8389919e6616e8ba2fce664f7391bcdbdc
Jan Szolno <jjszolno@gmail.com>
2024-06-06 15:54:20 -0300

    removes unused welcome screen

--- src/features/auth/welcome/screen/types.ts
+++
@@ -1,8 +0,0 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-
-import { AuthStackParamList, AuthStackScreens } from 'navigation/stacks/auth';
-
-export type WelcomeNavigationProps = NativeStackScreenProps<
-  AuthStackParamList,
-  AuthStackScreens.Welcome
->;

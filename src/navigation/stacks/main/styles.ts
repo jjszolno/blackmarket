bdb4991525308b3ea7b82e12e73755cc61c1e2c4
Jan Szolno <jjszolno@gmail.com>
2024-06-10 12:00:09 -0300

    Adds bottom tab navigation and screens

---
+++ src/navigation/stacks/main/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  bar: {
+    backgroundColor: '#000',
+  },
+  itemActive: {
+    borderRadius: 22,
+    height: 44,
+    width: 44,
+    backgroundColor: '#ffffff',
+    color: '#000',
+  },
+});
+
+export default styles;
